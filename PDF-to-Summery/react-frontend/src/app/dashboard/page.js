38f3023df27eaafe166fe92bbc05d41ef361aeb2
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-06-11 18:52:03 +0530

    Added new features

---
+++ PDF-to-Summery/react-frontend/src/app/dashboard/page.js
@@ -0,0 +1,213 @@
+"use client";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useRouter } from 'next/navigation';
+
+
+import React, { useState, useEffect } from "react";
+import pdfToText from "react-pdftotext";
+
+import logo from "@/images/logo.png";
+
+export default function Home() {
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [pdfData, setPdfData] = useState("");
+  const [Summary, setSummary] = useState("");
+  const [error, setError] = useState("");
+
+  const { push } = useRouter();
+
+  const [userPrompt, setUserPrompt] = useState("");
+
+  useEffect(() => {
+    if (!localStorage.getItem("Token")) {
+        push("/");
+    }
+    const difference = Date.now() - localStorage.getItem("Token");
+    console.log(difference)
+    if (difference > 3600000) {
+      push("/");
+    }
+}, []);
+
+  const fileAccepted = (event) => {
+    setFileName(event.target.files[0].name);
+    setFile(event.target.files[0]);
+    pdfToText(event.target.files[0]).then((text) => {
+      setPdfData(text);
+    });
+  };
+
+  const anotherSummary = () => {
+    setSummary("");
+    setFile("");
+    setFileName("");
+    setPdfData("");
+    setError("");
+    setLoading(false);
+  };
+
+
+  const generateSummary = () => {
+    if (!pdfData) {
+      setError("Please upload a file");
+      return;
+    }
+    setLoading(true);
+    fetch("https://textsummeryapisever.onrender.com/summarize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: pdfData,
+        userPrompt: userPrompt,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          setSummary(`
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <strong className="font-bold text-red-700">Error Occurred:</strong>
+      <span className="block sm:inline">${data.error}</span>
+    </div>`);
+          setLoading(false);
+          return;
+        }
+
+        // Replace double asterisk words with <b> tags
+        const boldRegex = /\*\*(.*?)\*\*/g;
+        const formattedText = data.replace(boldRegex, "<b>$1</b>");
+
+        const regex = /^\*(.*$)/gm;
+        const replacedText = formattedText.replace(regex, "<li>$1</li>");
+
+        setSummary(replacedText);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError("Error in generating Summary");
+        setLoading(false);
+      });
+  };
+
+  const handleChange = (event) => {
+    setUserPrompt(event.target.value);
+  };
+
+  const logOut = () => {    
+    localStorage.clear();
+    push("/");
+  }
+    
+  return (
+    <div className="flex flex-col h-screen items-center max-w-[700px] m-auto pt-5">
+      <div className="flex justify-center mt-2">
+        <Image src={logo} alt="Logo" height={100} width={100} />
+      </div>
+      <h1 className="text-3xl mt-5 font-bold">PDF to Summary</h1>
+      <div>
+        <p className="text-md mt-2 text-left">
+          Upload a PDF file and get a summary of it
+        </p>
+      </div>
+      {Summary ? (
+        <div className="w-full px-8 mt-5">
+          <p className="text-lg text-center font-semibold">Summary</p>
+          <p
+            dangerouslySetInnerHTML={{ __html: Summary }}
+            className="text-md mt-5 text-left whitespace-pre-line "
+          ></p>
+          <div className="w-fit m-auto mt-10">
+            <Button className="mt-10 m-auto" onClick={() => anotherSummary()}>
+              Generate Another Summary
+            </Button>
+          </div>
+        </div>
+      ) : loading ? (
+        <>
+        <p className="text-lg text-center mt-10 font-semibold">
+            Loading Summary Please Wait
+          </p>
+        <div className="m-auto">
+          
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 m-auto mt-5"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+        </div>
+        </>
+      ) : (
+        <div className="w-full">
+          <div className="w-full px-8">
+            {file ? (
+              <p className="text-lg text-center mt-10 font-semibold">
+                {fileName ? `Uploaded file : ${fileName}` : ""}
+              </p>
+            ) : (
+              <>
+                <Input
+                  type="file"
+                  accept=".pdf"
+                  onChange={fileAccepted}
+                  className="max-w-[650px] pt-1.5 mt-10 mx-auto"
+                />
+                {error ? (
+                  <p className="text-sm text-left mx-3 mt-1 font-semibold text-red-600">
+                    {error}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </>
+            )}
+          </div>
+          <div className="mt-5 w-full p-8">
+            <Label htmlFor="title">
+              You can provide optional instructions to create summary in text
+              box below!!
+            </Label>
+            <Textarea
+              id="title"
+              type="text"
+              onChange={handleChange}
+              className="mt-2"
+            />
+          </div>
+          <div className="w-fit m-auto">
+            <Button className="mt-5" onClick={() => generateSummary()}>
+              Generate Summary
+            </Button>
+          </div>
+        </div>
+      )}
+
+      <p className="m-5 text-sm text-center mt-10">Click on log out to clear clear the session <br/> <span className="text-red-400 text-sm" onClick={logOut}>Log Out</span> </p>
+
+      <p className=" text-center text-sm pb-10">
+        Created by{" "}
+        <a className=" text-orange-300" href="https://ai-guru-kul.vercel.app">
+          AIGurukul
+        </a>
+      </p>
+    </div>
+  );
+}
--- PDF-to-Summery/react-frontend/src/app/page.js
+++ PDF-to-Summery/react-frontend/src/app/page.js
@@ -1,162 +1,75 @@
-"use client";
-import Image from "next/image";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+"use client"
 
-import React, { useState, useEffect } from "react";
-import pdfToText from "react-pdftotext";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react"
+import { useRouter } from 'next/navigation';
 
-import logo from "@/images/logo.png";
+export default function Page() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+  const { push } = useRouter();
 
-export default function Home() {
-  const [file, setFile] = useState();
-  const [fileName, setFileName] = useState("");
-
-  const [loading, setLoading] = useState(false);
-  const [pdfData, setPdfData] = useState("");
-  const [summery, setSummery] = useState("");
-  const [error, setError] = useState('')
-
-  const [userPrompt, setUserPrompt] = useState('')
-
-  const fileAccepted = (event) => {
-    setFileName(event.target.files[0].name);
-    setFile(event.target.files[0]);
-    pdfToText(event.target.files[0]).then((text) => {
-      setPdfData(text);
-    });
-  };
-
-  const anotherSummery = () => {
-    setSummery("");
-    setFile("")
-    setFileName("")
-    setPdfData("")
-    setError("")
-    setLoading(false)
+  const handleChangeUsername = (event) => {
+    setUsername(event.target.value)
   }
 
-  const generateSummery = () => {
-    if (!pdfData) {
-      setError('Please upload a file')
-      return;
-    }
-    setLoading(true);
-    fetch("https://textsummeryapisever-production.up.railway.app/summarize", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        data: pdfData,
-        userPrompt : userPrompt,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if(data.error){
-          setSummery(`
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-      <strong className="font-bold text-red-700">Error Occurred:</strong>
-      <span className="block sm:inline">${data.error}</span>
-    </div>`);
-          setLoading(false)
-          return
+  const handleChangePassword = (event) => {
+    console.log(event.target.value)
+    setPassword(event.target.value)
+  }
+
+  const onSignIn = () => {
+    if(username == "user" && password == "user@password"){
+      localStorage.setItem("Token",Date.now())
+      setError(null)
+      return push('/dashboard')
+   }else{
+    setError("Invalid username or password")
+   }
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("Token")) {
+        const difference = localStorage.getItem("Token") - Date.now();
+        if (difference < 3600000) {
+          push("/dashboard");
         }
+    }
+}, []);
 
-        // Replace double asterisk words with <b> tags
-        const boldRegex = /\*\*(.*?)\*\*/g;
-        const formattedText = data.replace(boldRegex, "<b>$1</b>");
-
-        const regex = /^\*(.*$)/gm;
-        const replacedText = formattedText.replace(regex, "<li>$1</li>");
-
-        setSummery(replacedText);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError('Error in generating Summery')
-        setLoading(false);
-      });
-  };
-
-  const handleChange = (event) => {
-    setUserPrompt(event.target.value)
-  }
   return (
-    <div className="flex flex-col h-screen items-center max-w-[700px] m-auto mt-10">
-      <div className="flex justify-center mt-2">
-        <Image src={logo} alt="Logo" height={100} width={100} />
-      </div>
-      <h1 className="text-3xl mt-5 font-bold">PDF to Summery</h1>
-      <div>
-        <p className="text-md mt-2 text-left">
-          Upload a PDF file and get a summary of it
-        </p>
-      </div>
-      {summery ? (
-        <div className="w-full px-8 mt-5">
-          <p className="text-lg text-center font-semibold">Summery</p>
-          <p
-            dangerouslySetInnerHTML={{ __html: summery }}
-            className="text-md mt-5 text-left whitespace-pre-line "
-          ></p>
-          <div className="w-fit m-auto mt-10">
-          <Button className="mt-10 m-auto" onClick={() => anotherSummery()}>
-            Generate Another Summery
-          </Button>
+    <div className="flex justify-center items-center h-screen">
+      <Card className='max-w-[600px] border-none'>
+      <CardTitle className="text-2xl text-center">Welcome to PDF to Summary </CardTitle>
+        <CardHeader>
+          <CardTitle className="text-xl text-center">Login</CardTitle>
+          <CardDescription className=' text-center'>Please enter your username and password to login.</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="username">Username</Label>
+            <Input id="username" type="text" placeholder="Your username" required onChange={handleChangeUsername} />
           </div>
-        </div>
-      ) : loading ? (
-        <div>
-          <p className="text-lg text-center mt-10 font-semibold">
-            Loading Summery Please Wait
-          </p>
-          <p className="text-center"> Loading .... </p>
-        </div>
-      ) : (
-        <div className="w-full">
-          <div className="w-full px-8">
-            {file ? (
-              <p className="text-lg text-center mt-10 font-semibold">
-                {fileName ? `Uploaded file : ${fileName}` : ""}
-              </p>
-            ) : (
-              <>
-              <Input
-                type="file"
-                accept=".pdf"
-                onChange={fileAccepted}
-                className="max-w-[650px] pt-1.5 mt-10 mx-auto"
-              />
-              {
-                error ? (
-                  <p className="text-sm text-left mx-3 mt-1 font-semibold text-red-600">
-                    {error}
-                  </p>
-                ) : ''
-              }
-              </>
-            )}
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" placeholder="Your password" required onChange={handleChangePassword} />
           </div>
-          <div className="mt-5 w-full p-8">
-            <Label htmlFor="title">
-              You can give instructions or focus point on which Summery will be
-              based
-            </Label>
-            <Textarea id="title" type="text" onChange={handleChange} className="mt-2" />
-          </div>
-          <div className="w-fit m-auto">
-            <Button className="mt-5" onClick={() => generateSummery()}>
-              Generate Summery
-            </Button>
-          </div>
-        </div>
-      )}
-
-      <p className="mt-10 text-center text-sm pb-10">Created by AIGurukul</p>
+          <p className="text-sm text-red-500">{error}</p>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full hover:bg-white hover:text-black transition-colors" onClick={onSignIn} >Sign in</Button>
+        </CardFooter>
+        <p className=" mt-8 text-center text-sm pb-10">
+        Created by{" "}
+        <a className=" text-orange-300" href="https://ai-guru-kul.vercel.app">
+          AIGurukul
+        </a>
+      </p>
+      </Card>
     </div>
-  );
+  )
 }
